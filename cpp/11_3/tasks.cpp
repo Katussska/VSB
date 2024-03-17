26f7aea5f07ae67267025590046711ae32045255
Katussska <katkabaier@gmail.com>
2024-03-17 17:05:26 +0100

    Gonna cryy T-T

--- cpp/11_3/tasks.cpp
+++ cpp/11_3/tasks.cpp
@@ -1,60 +1,173 @@
 #include "tasks.h"
 
-Integer::Integer(int value) {
+Integer::Integer(int value) : number(value) {}
 
+Integer *Integer::clone() const {
+    //TODO:
+    return nullptr;
+}
+
+void Integer::accept(class Visitor &visitor) {
+    //TODO:
+}
+
+const Value &Integer::operator[](size_t index) const {
+    throw std::runtime_error("Indexing not supported.");
+}
+
+const Value &Integer::operator[](const std::string &key) const {
+    throw std::runtime_error("Indexing not supported.");
 }
 
 int Integer::get_value() {
+    //TODO:
     return 0;
+}
+
+void Integer::accept(Visitor &visitor) const {
+    //TODO:
 }
 
 Array::Array(const std::vector<Value *> &values) {
-
-}
-
-int Array::size() {
-    return 0;
+    //TODO:
 }
 
 void Array::append(Value *value) {
+    //TODO:
 }
 
-void Array::remove(int index) {
-
-
-}
-
-const Value *Array::operator[](int index) const {
+Array *Array::clone() const {
+    //TODO:
     return nullptr;
 }
+
+void Array::accept(class Visitor &visitor) {
+    //TODO:
+}
+
+const Value &Array::operator[](const std::string &key) const {
+    throw std::runtime_error("Indexing not supported.");
+}
+
+const Value &Array::operator[](size_t index) const {
+    if (index < values.size())
+        return *(values[index]);
+    else
+        throw std::runtime_error("Index out of Array's size.");
+}
+
+int Array::size() {
+    //TODO:
+    return 0;
+}
+
+[[maybe_unused]] void Array::remove(size_t index) {
+    //TODO:
+}
+
+void Array::accept(Visitor &visitor) const {
+    //TODO:
+}
+
+Array::~Array() = default;
 
 Array::Array() = default;
 
 Object::Object(const std::unordered_map<std::string, Value *> &data) {
-
-}
-
-std::vector<std::string> Object::keys() {
-    return {};
+    //TODO:
 }
 
 void Object::insert(const std::string &str, Value *value) {
-
+    //TODO:
 }
 
 void Object::remove(const std::string &key) {
-
+    //TODO:
 }
 
-int Object::size() {
-    return 0;
-}
-
-const Value *Object::operator[](const std::string &key) const {
+Object *Object::clone() const {
     return nullptr;
 }
 
-Object::Object() = default;
+void Object::accept(class Visitor &visitor) {
+    //TODO:
+}
+
+const Value &Object::operator[](size_t index) const {
+    throw std::runtime_error("Indexing not supported.");
+}
+
+const Value &Object::operator[](const std::string &key) const {
+    auto temp = data.find(key);
+
+    if (temp != data.end())
+        return *(temp->second);
+    else
+        throw std::runtime_error("Key not found.");
+}
+
+int Object::size() {
+    //TODO:
+    return 0;
+}
+
+std::vector<std::string> Object::keys() {
+    //TODO:
+    return {};
+}
+
+void Object::accept(Visitor &visitor) const {
+    //TODO:
+}
 
 Object::~Object() = default;
 
+Object::Object() = default;
+
+Null *Null::clone() const {
+    //TODO:
+    return nullptr;
+}
+
+void Null::accept(class Visitor &visitor) {
+}
+
+const Value &Null::operator[](size_t index) const {
+    throw std::runtime_error("Indexing not supported.");
+}
+
+const Value &Null::operator[](const std::string &key) const {
+    throw std::runtime_error("Indexing not supported.");
+}
+
+void Null::accept(Visitor &visitor) const {
+    //TODO:
+}
+
+[[maybe_unused]] void RemoveNullVisitor::visit(Null &null) {
+    //TODO:
+}
+
+[[maybe_unused]] void RemoveNullVisitor::visit(Array &array) {
+    //TODO:
+}
+
+[[maybe_unused]] void RemoveNullVisitor::visit(Object &object) {
+    //TODO:
+}
+
+void PrintVisitor::visit(const Integer &integer) {
+    //TODO:
+}
+
+void PrintVisitor::visit(const Null &null) {
+    //TODO:
+}
+
+void PrintVisitor::visit(const Array &array) {
+    //TODO:
+}
+
+void PrintVisitor::visit(const Object &object) {
+    //TODO:
+}
--- cpp/11_3/tasks.h
+++ cpp/11_3/tasks.h
@@ -3,8 +3,23 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <iostream>
+
+class Visitor; // Forward declaration
 
 class Value {
+public:
+    [[nodiscard]] virtual Value *clone() const = 0;
+
+    virtual void accept(Visitor &visitor) const = 0;
+
+    virtual void accept(Visitor &visitor) = 0;
+
+    virtual const Value &operator[](size_t index) const = 0;
+
+    virtual const Value &operator[](const std::string &key) const = 0;
+
+    virtual ~Value() = default;
 };
 
 class Integer : public Value {
@@ -16,12 +31,22 @@ public:
 
     [[nodiscard]] static int get_value();
 
+    [[nodiscard]] Integer *clone() const override;
+
+    void accept(Visitor &visitor) const override;
+
+    void accept(Visitor &visitor) override;
+
+    const Value &operator[](size_t index) const override;
+
+    const Value &operator[](const std::string &key) const override;
+
+    ~Integer() override = default;
 };
 
 class Array : public Value {
 private:
     std::vector<Value *> values;
-    size_t values_count{};
 
 public:
     Array();
@@ -32,15 +57,24 @@ public:
 
     void append(Value *value);
 
-    void remove(int index);
+    [[maybe_unused]] void remove(size_t index);
 
-    const Value *operator[](int index) const;
+    [[nodiscard]] Array *clone() const override;
+
+    void accept(Visitor &visitor) const override;
+
+    void accept(Visitor &visitor) override;
+
+    const Value &operator[](size_t index) const override;
+
+    const Value &operator[](const std::string &key) const override;
+
+    ~Array() override;
 };
 
 class Object : public Value {
 private:
     std::unordered_map<std::string, Value *> data;
-    size_t data_count{};
 
 public:
     Object();
@@ -51,14 +85,75 @@ public:
 
     static std::vector<std::string> keys();
 
-    void insert(const std::string &str, Value *value);
+    void insert(const std::string &key, Value *value);
 
     void remove(const std::string &key);
 
-    const Value *operator[](const std::string &key) const;
+    Object *clone() const override;
 
-    virtual ~Object();
+    void accept(Visitor &visitor) const override;
+
+    void accept(Visitor &visitor) override;
+
+    const Value &operator[](size_t index) const override;
+
+    const Value &operator[](const std::string &key) const override;
+
+    ~Object() override;
 };
 
 class Null : public Value {
+public:
+    [[nodiscard]] Null *clone() const override;
+
+    void accept(Visitor &visitor) const override;
+
+    void accept(Visitor &visitor) override;
+
+    const Value &operator[](size_t index) const override;
+
+    const Value &operator[](const std::string &key) const override;
+};
+
+class Visitor {
+public:
+    virtual void visit(const Integer &integer) = 0;
+
+    virtual void visit(const Array &array) = 0;
+
+    virtual void visit(const Object &object) = 0;
+
+    virtual void visit(const Null &null) = 0;
+
+    virtual ~Visitor() = default;
+};
+
+class RemoveNullVisitor : public Visitor {
+public:
+    using Visitor::visit;
+
+    [[maybe_unused]] void visit([[maybe_unused]] Null &null);
+
+    [[maybe_unused]] void visit(Array &array);
+
+    [[maybe_unused]] void visit(Object &object);
+
+    ~RemoveNullVisitor() override = default;
+};
+
+class PrintVisitor : public Visitor {
+private:
+    std::stringstream &stream;
+public:
+    explicit PrintVisitor(std::stringstream &stream);
+
+    void visit(const Integer &integer) override;
+
+    void visit(const Null &null) override;
+
+    void visit(const Array &array) override;
+
+    void visit(const Object &object) override;
+
+    ~PrintVisitor() override = default;
 };
