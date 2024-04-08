8b5c1da61d30fe6301b92fa910f1b3fcba26c3c8
Katussska <katkabaier@gmail.com>
2024-04-08 14:46:11 +0200

    init

--- javaII/task2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cz.vsb {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-	requires static lombok;
-	requires org.apache.logging.log4j;
-    opens cz.vsb to javafx.fxml;
-    exports cz.vsb;
-}
